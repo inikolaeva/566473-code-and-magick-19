0d24e3c03ab2d864f0ab53ae22941a9b7698e0aa
inikolaeva <ityurinka@gmail.com>
2020-02-02 18:18:40 +0300

    Update setup.js
    comments fixed

--- js/setup.js
+++ js/setup.js
@@ -6,26 +6,29 @@ var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var RANDOM_WIZARD_COUNT = 4;
 
+var userDialog = document.querySelector('.setup');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
+
 var getRandomValueFromArray = function (arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 };
 
-var generateWizardsArray = function (count) {
-  var wizards = [];
-  for (var i = 0; i < count; i++) {
-    var obj = {};
-    obj.name = getRandomValueFromArray(WIZARD_NAMES) + ' ' + getRandomValueFromArray(WIZARD_SURNAME);
-    obj.coatColor = getRandomValueFromArray(COAT_COLOR);
-    obj.eyesColor = getRandomValueFromArray(EYES_COLOR);
-    wizards[i] = obj;
-  }
-  return wizards;
+var generateWizard = function () {
+  var obj = {};
+  obj.name = getRandomValueFromArray(WIZARD_NAMES) + ' ' + getRandomValueFromArray(WIZARD_SURNAME);
+  obj.coatColor = getRandomValueFromArray(COAT_COLOR);
+  obj.eyesColor = getRandomValueFromArray(EYES_COLOR);
+  return obj;
 };
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
@@ -33,19 +36,9 @@ var renderWizard = function (wizard) {
 };
 
 
-var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-
-var fragment = document.createDocumentFragment();
-var wizards = generateWizardsArray(RANDOM_WIZARD_COUNT);
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+for (var i = 0; i < RANDOM_WIZARD_COUNT; i++) {
+  fragment.appendChild(renderWizard(generateWizard()));
 }
 similarListElement.appendChild(fragment);
-
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
